99075ba2458f3959eec036a424236d6a91a34887
adamwebster <adamwebster@me.com>
2020-06-08 11:59:10 -0400

    Started work on homepage

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,7 +1,19 @@
-
 <script>
-    import Logo from '../Logo/Logo.svelte';
+  import Logo from "../Logo/Logo.svelte";
 </script>
-<header>
-    <Logo classNames="awm-header-logo" width="44px"/>
+
+<style>
+  .awm-header {
+    border-bottom: solid 1px var(--border-color);
+  }
+  .awm-header-inner {
+    width: 1080px;
+    margin: 0 auto;
+  }
+</style>
+
+<header class="awm-header">
+  <div class="awm-header-inner">
+    <Logo classNames="awm-header-logo" width="30px" />
+  </div>
 </header>
