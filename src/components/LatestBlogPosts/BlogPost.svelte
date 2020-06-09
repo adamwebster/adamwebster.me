84fe90441a2014ec71f0be038099ffe1d5f8c81b
Adam Webster <adamwebster@me.com>
2020-06-09 07:02:48 -0400

    Updates to the components that are used to build the homepage

---
+++ src/components/LatestBlogPosts/BlogPost.svelte
@@ -0,0 +1,47 @@
+<style>
+  .awm-list-item {
+    background-color: #fff;
+    border: solid 1px var(--border-color);
+    border-radius: 5px;
+  }
+
+  .awm-list-item img {
+    object-fit: cover;
+    width: 100%;
+    height: 150px;
+  }
+
+  .awm-list-item-post-content {
+    padding: 10px;
+  }
+
+  h2 {
+    text-transform: uppercase;
+    font-size: 20px;
+    margin: 0;
+  }
+
+  h2 a {
+    text-decoration: none;
+  }
+
+  .date {
+    font-size: 13px;
+  }
+</style>
+
+<div class="awm-list-item">
+  <img
+    alt="img1"
+    src="https://drscdn.500px.org/photo/163833279/q%3D80_m%3D2000/v2?sig=3dca74cf8cd24a1adf31367f43dbcac9adfcff4eb3a9c1f25919dfa0aea39d0d" />
+  <article class="awm-list-item-post-content">
+    <h2>
+      <a href="blog/title-of-post">Title of the Post</a>
+    </h2>
+    <div class="date">02/02/2020</div>
+    <p>
+      This is some text of a blog post and would either be longer or shorter
+      then this...
+    </p>
+  </article>
+</div>
--- src/components/LatestBlogPosts/LatestBlogPosts.svelte
+++ src/components/LatestBlogPosts/LatestBlogPosts.svelte
@@ -1,5 +1,9 @@
 <script>
   import Button from "../Button/Button.svelte";
+  import BlogPost from "./BlogPost.svelte";
+  const HandleClick = () => {
+    alert("click");
+  };
 </script>
 
 <style>
@@ -10,7 +14,6 @@
     font-weight: 300;
     text-transform: uppercase;
   }
-
   .awm-section-wrapper {
     display: flex;
     width: 100%;
@@ -19,7 +22,6 @@
     box-sizing: border-box;
     flex-flow: column;
   }
-
   .awm-section-inner {
     flex-flow: row;
     display: flex;
@@ -32,76 +34,20 @@
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 20px;
   }
-
-  .awm-list-item {
-    background-color: #fff;
-    border: solid 1px var(--border-color);
-    border-radius: 5px;
-  }
-
-  .awm-list-item img {
-    object-fit: cover;
-    width: 100%;
-    height: 150px;
-  }
-
-  .awm-list-item-post-content {
-    padding: 10px;
-  }
   .awm-list-more {
     display: flex;
     align-content: center;
     justify-content: center;
     padding: 20px;
   }
-
-  h2 {
-    margin: 0;
-  }
 </style>
 
 <section class="awm-section-wrapper" id="LatestBlogPosts">
   <div class="awm-section-inner">
     <div class="awm-list-of-posts">
-      <div class="awm-list-item">
-        <img
-          alt="img1"
-          src="https://drscdn.500px.org/photo/163833279/q%3D80_m%3D2000/v2?sig=3dca74cf8cd24a1adf31367f43dbcac9adfcff4eb3a9c1f25919dfa0aea39d0d" />
-        <article class="awm-list-item-post-content">
-          <h2>Title of the Post</h2>
-          <div>02/02/2020</div>
-          <p>
-            This is some text of a blog post and would either be longer or
-            shorter then this...
-          </p>
-        </article>
-      </div>
-      <div class="awm-list-item">
-        <img
-          alt="img1"
-          src="https://drscdn.500px.org/photo/163833279/q%3D80_m%3D2000/v2?sig=3dca74cf8cd24a1adf31367f43dbcac9adfcff4eb3a9c1f25919dfa0aea39d0d" />
-        <article class="awm-list-item-post-content">
-          <h2>Title of the Post</h2>
-          <div>02/02/2020</div>
-          <p>
-            This is some text of a blog post and would either be longer or
-            shorter then this...
-          </p>
-        </article>
-      </div>
-      <div class="awm-list-item">
-        <img
-          alt="img1"
-          src="https://drscdn.500px.org/photo/163833279/q%3D80_m%3D2000/v2?sig=3dca74cf8cd24a1adf31367f43dbcac9adfcff4eb3a9c1f25919dfa0aea39d0d" />
-        <article class="awm-list-item-post-content">
-          <h2>Title of the Post</h2>
-          <div>02/02/2020</div>
-          <p>
-            This is some text of a blog post and would either be longer or
-            shorter then this...
-          </p>
-        </article>
-      </div>
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
     </div>
     <div>
       <h1>
@@ -112,6 +58,6 @@
     </div>
   </div>
   <div class="awm-list-more">
-    <Button>Read More</Button>
+    <Button primary on:click={HandleClick}>Read More</Button>
   </div>
 </section>
