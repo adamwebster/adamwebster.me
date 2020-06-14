44e916d6f3a079b363e390121fcc7312de8a91a5
Adam Webster <adamwebster@me.com>
2020-06-14 18:21:37 -0400

    Added categories to the the blog settings

--- src/SiteSettings.js
+++ src/SiteSettings.js
@@ -4,4 +4,9 @@ export const BlogSettings = {
 
 export const PortfolioSettings = {
   postsPerPage: 9,
+  categories: [
+    { name: "UI Design", bgColor: "tomato", borderColor: "#fff" },
+    { name: "App Design", bgColor: "LemonChiffon", borderColor: "Black" },
+    { name: "Website Design", bgColor: "PeachPuff", borderColor: "Black" },
+  ],
 };
