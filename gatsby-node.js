24e48feaa9313d85c3efc3d00a37772632260804
adamwebster <adamwebster@me.com>
2020-06-16 11:51:40 -0400

    Added portfolio template pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-// const path = require(`path`)
-// const _ = require("lodash")
+const path = require(`path`)
+const _ = require("lodash")
 
 // exports.sourceNodes = ({ actions, schema }) => {
 //     const { createTypes } = actions
@@ -72,107 +72,107 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     }
 }
 
-// exports.createPages = async ({ actions, graphql, reporter }) => {
-//     const { createPage } = actions
-//     const blogPostTemplate = path.resolve(`src/pages/BlogPost/BlogPost.jsx`)
-//     const portfolioItemTemplate = path.resolve(`src/pages/PortfolioItem/PortfolioItem.jsx`)
-//     const categoryTemplate = path.resolve(`src/pages/Blog/category.js`)
-//     const tagTemplate = path.resolve(`src/pages/Blog/tag.js`)
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const { createPage } = actions
+    const blogPostTemplate = path.resolve(`src/templates/BlogPost.tsx`)
+    const portfolioItemTemplate = path.resolve(`src/templates/PortfolioPost.tsx`)
+    // const categoryTemplate = path.resolve(`src/pages/Blog/category.js`)
+    // const tagTemplate = path.resolve(`src/pages/Blog/tag.js`)
 
-//     const result = await graphql(`
-//     {
-//       allMdx(
-//         sort: { order: DESC, fields: [frontmatter___date] }
-//         limit: 1000
-//       ) {
-//         edges {
-//           node {
-//             frontmatter {
-//               path
-//               category
-//               tags
-//             }
-//             fields {
-//               sourceInstanceName
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `)
-//     // Handle errors
-//     if (result.errors) {
-//         reporter.panicOnBuild(`Error while running GraphQL query.`)
-//         return
-//     }
+    const result = await graphql(`
+    {
+      allMdx(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+              category
+              tags
+            }
+            fields {
+              sourceInstanceName
+            }
+          }
+        }
+      }
+    }
+  `)
+    // Handle errors
+    if (result.errors) {
+        reporter.panicOnBuild(`Error while running GraphQL query.`)
+        return
+    }
 
-//     const posts = result.data.allMdx.edges.filter(item => item.node.fields.sourceInstanceName === 'blog-post');
-//     const postsPerPage = 10;
-//     const numPages = Math.ceil(posts.length / postsPerPage)
-//     Array.from({ length: numPages }).forEach((_, i) => {
-//         createPage({
-//             path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-//             component: path.resolve("./src/templates/BlogTemplate.js"),
-//             context: {
-//                 limit: postsPerPage,
-//                 skip: i * postsPerPage,
-//                 numPages,
-//                 currentPage: i + 1,
-//             },
-//         })
-//     })
+    const posts = result.data.allMdx.edges.filter(item => item.node.fields.sourceInstanceName === 'blog-post');
+    const postsPerPage = 10;
+    const numPages = Math.ceil(posts.length / postsPerPage)
+    Array.from({ length: numPages }).forEach((_, i) => {
+        createPage({
+            path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+            component: path.resolve("./src/templates/BlogPage.tsx"),
+            context: {
+                limit: postsPerPage,
+                skip: i * postsPerPage,
+                numPages,
+                currentPage: i + 1,
+            },
+        })
+    })
 
-//     const portfolioPosts = result.data.allMdx.edges.filter(item => item.node.fields.sourceInstanceName === 'portfolio-item');
-//     const portfolioPostsPerPage = 10;
-//     const numPagesPort = Math.ceil(portfolioPosts.length / portfolioPostsPerPage)
-//     Array.from({ length: numPagesPort }).forEach((_, i) => {
-//         createPage({
-//             path: i === 0 ? `/portfolio` : `/portfolio/${i + 1}`,
-//             component: path.resolve("./src/templates/PortfolioTemplate.js"),
-//             context: {
-//                 limit: portfolioPostsPerPage,
-//                 skip: i * portfolioPostsPerPage,
-//                 numPagesPort,
-//                 currentPage: i + 1,
-//             },
-//         })
-//     })
+    const portfolioPosts = result.data.allMdx.edges.filter(item => item.node.fields.sourceInstanceName === 'portfolio-item');
+    const portfolioPostsPerPage = 10;
+    const numPagesPort = Math.ceil(portfolioPosts.length / portfolioPostsPerPage)
+    Array.from({ length: numPagesPort }).forEach((_, i) => {
+        createPage({
+            path: i === 0 ? `/portfolio` : `/portfolio/${i + 1}`,
+            component: path.resolve("./src/templates/PortfolioPage.tsx"),
+            context: {
+                limit: portfolioPostsPerPage,
+                skip: i * portfolioPostsPerPage,
+                numPagesPort,
+                currentPage: i + 1,
+            },
+        })
+    })
 
-//     result.data.allMdx.edges.forEach(({ node }) => {
-//         if (node.fields.sourceInstanceName === 'blog-post') {
-//             createPage({
-//                 path: node.frontmatter.path,
-//                 component: blogPostTemplate,
-//                 context: {}, // additional data can be passed via context
-//             })
-//             if (node.frontmatter.category) {
-//                 createPage({
-//                     path: `/blog/category/${_.kebabCase(node.frontmatter.category)}/`,
-//                     component: categoryTemplate,
-//                     context: {
-//                         category: node.frontmatter.category
-//                     },
+    result.data.allMdx.edges.forEach(({ node }) => {
+        if (node.fields.sourceInstanceName === 'blog-post') {
+            createPage({
+                path: node.frontmatter.path,
+                component: blogPostTemplate,
+                context: {}, // additional data can be passed via context
+            })
+            // if (node.frontmatter.category) {
+            //     createPage({
+            //         path: `/blog/category/${_.kebabCase(node.frontmatter.category)}/`,
+            //         component: categoryTemplate,
+            //         context: {
+            //             category: node.frontmatter.category
+            //         },
 
-//                 })
-//             }
-//             if (node.frontmatter.tags) {
-//                 node.frontmatter.tags.forEach(tag => {
-//                     createPage({
-//                         path: `/blog/tag/${_.kebabCase(tag)}/`,
-//                         component: tagTemplate,
-//                         context: {
-//                             tag: tag,
-//                         },
-//                     })
-//                 })
-//             }
-//         }
-//         if (node.fields.sourceInstanceName === 'portfolio-item') {
-//             createPage({
-//                 path: node.frontmatter.path,
-//                 component: portfolioItemTemplate,
-//                 context: {}, // additional data can be passed via context
-//             })
-//         }
-//     })
-// }
+            //     })
+            // // }
+            // if (node.frontmatter.tags) {
+            //     node.frontmatter.tags.forEach(tag => {
+            //         createPage({
+            //             path: `/blog/tag/${_.kebabCase(tag)}/`,
+            //             component: tagTemplate,
+            //             context: {
+            //                 tag: tag,
+            //             },
+            //         })
+            //     })
+            // }
+        }
+        if (node.fields.sourceInstanceName === 'portfolio-item') {
+            createPage({
+                path: node.frontmatter.path,
+                component: portfolioItemTemplate,
+                context: {}, // additional data can be passed via context
+            })
+        }
+    })
+}
---
+++ src/templates/PortfolioPost.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Layout } from "../components/Layout";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { MDXProvider } from "@mdx-js/react";
+
+interface Props {
+  data: any;
+}
+const PortfolioPost = ({ data }: Props) => {
+  const {
+    mdx: { frontmatter, body },
+  } = data;
+  return (
+    <Layout>
+      <h1> {frontmatter.title}</h1>
+      <MDXProvider components={{}}>
+        <MDXRenderer>{body}</MDXRenderer>
+      </MDXProvider>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    mdx(frontmatter: { path: { eq: $path } }) {
+      body
+      frontmatter {
+        date
+        path
+        title
+        tags
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        category
+      }
+    }
+  }
+`;
+
+export default PortfolioPost;
