7c87ff02dc910262000769cd5804a11228256a66
adamwebster <adamwebster@me.com>
2020-06-19 15:54:59 -0400

    Working on redux stores

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,12 +2,16 @@ import React, { useEffect } from 'react';
 import { Layout } from '../components/Layout';
 import { useDispatch } from 'react-redux';
 import { setHeaderColor } from '../state/actions';
+import { Colors } from '@adamwebster/fused-components';
 
 const Contact = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setHeaderColor('tomato'));
+    return () => {
+      dispatch(setHeaderColor(Colors.primary));
+    };
   }, []);
 
   return (
