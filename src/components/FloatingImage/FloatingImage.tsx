d17dd55f6379a0899b37c07f815df3143e50e6db
Adam Webster <adamwebster@me.com>
2020-07-09 08:29:47 -0400

    Added a floating image component

---
+++ src/components/FloatingImage/FloatingImage.tsx
@@ -0,0 +1,69 @@
+import React, { ReactNode } from 'react';
+import styled, { css } from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+const StyledFloatingImage = styled.div<Props>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  float: ${({ float }) => float};
+  overflow: hidden;
+  margin-bottom: 15px;
+  ${({ float }) =>
+    float === 'right'
+      ? css`
+          margin-left: 30px;
+        `
+      : css`
+          margin-right: 30px;
+        `}
+  p,figure {
+    margin: 0;
+  }
+`;
+interface Props {
+  float?: string;
+  width?: string;
+  height?: string;
+  imageFilename?: string;
+  caption?: string;
+}
+const FloatingImage = ({
+  float = 'left',
+  width = '50%',
+  height = 'auto',
+  imageFilename = 'default.jpg',
+  caption,
+}: Props) => {
+  const ImageQuery = useStaticQuery(graphql`
+    query {
+      allImageSharp {
+        edges {
+          node {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
+              originalName
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log(imageFilename);
+
+  const image = ImageQuery.allImageSharp.edges.filter(
+    (img: any) => img.node.fluid.originalName === imageFilename
+  );
+  console.log(image);
+
+  return (
+    <StyledFloatingImage float={float} width={width} height={height}>
+      <figure>
+        <Img fluid={image[0].node.fluid} />
+        {caption && <figcaption>{caption}</figcaption>}
+      </figure>
+    </StyledFloatingImage>
+  );
+};
+
+export default FloatingImage;
